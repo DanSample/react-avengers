fc5406915f2c3002e59cd266ff083e216a4bf2e9
Dan Sample <danssample@gmail.com>
2019-05-21 21:04:53 -0700

    intitial commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      avengers: [
+        { name: 'Captain America', id: 1 },
+        { name: 'Iron Man', id: 2 },
+        { name: 'Thor', id: 3 }
+      ]
+    };
+  }
 
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Avengers: by Dan Sample</p>
+          <div>
+            {this.state.avengers.map(a => (
+              <div key={a.id}>
+                <p>Name: {a.name}</p>
+              </div>
+            ))}
+          </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
 export default App;
